2ac98afb3b17b26aedaf0b47d443dd7f4a5ebe28
dmndpl <diamandopol.andrei10@gmail.com>
2024-01-13 14:27:38 +0000

    Format

--- video_capture.py
+++ video_capture.py
@@ -5,6 +5,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 SECONDS_TO_SKIP = 5
 
+
 class VideoCapture():
 
     # Variables to store the coordinates of the selected ROI
@@ -20,13 +21,13 @@ class VideoCapture():
         self.path = path
 
         # We can either specify the cropping, use the crop wizard, or not crop at all
-        # If we don't want to crop, we'll set the crop factor as the whole image. 
+        # If we don't want to crop, we'll set the crop factor as the whole image.
         # If we want to crop, and the crop factors are not set, we'll use the wizard
         # else we assume crop factors were provided
         if not crop:
             self.roi_selected = False
             logging.info("Using uncropped capture")
-            self.roi_start, self.roi_end = (0,0), self._get_roi_end()
+            self.roi_start, self.roi_end = (0, 0), self._get_roi_end()
             self.roi_selected = True
         elif not roi_start or not roi_end:
             self.roi_selected = False
@@ -84,7 +85,8 @@ class VideoCapture():
 
             # Draw rectangle while selecting
             if self.roi_selected:
-                cv2.rectangle(img_display, self.roi_start, self.roi_end, (0, 255, 0), 2)
+                cv2.rectangle(img_display, self.roi_start,
+                              self.roi_end, (0, 255, 0), 2)
 
             cv2.imshow('Select ROI', img_display)
             key = cv2.waitKey(1) & 0xFF
@@ -96,13 +98,16 @@ class VideoCapture():
             # Press 'c' to confirm and store the ROI coordinates
             elif key == ord('c'):
                 # Ensure correct coordinates order for the region of interest
-                x1, y1 = min(self.roi_start[0], self.roi_end[0]), min(self.roi_start[1], self.roi_end[1])
-                x2, y2 = max(self.roi_start[0], self.roi_end[0]), max(self.roi_start[1], self.roi_end[1])
+                x1, y1 = min(self.roi_start[0], self.roi_end[0]), min(
+                    self.roi_start[1], self.roi_end[1])
+                x2, y2 = max(self.roi_start[0], self.roi_end[0]), max(
+                    self.roi_start[1], self.roi_end[1])
                 selected_image = image[y1:y2, x1:x2]
 
                 # You can further process or save the selected_roi here
                 cv2.imshow('Selected ROI', selected_image)
-                cv2.waitKey(0)  # Wait for any key press before closing the ROI window
+                # Wait for any key press before closing the ROI window
+                cv2.waitKey(0)
                 cv2.destroyWindow('Selected ROI')
                 logging.info("Crop selected")
                 break
@@ -122,16 +127,19 @@ class VideoCapture():
             self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
             self.fps = self.cap.get(cv2.CAP_PROP_FPS)
 
-            logging.info("Video capture has a total of %s frames at %s frames per second", self.total_frames, self.fps)
-        
+            logging.info(
+                "Video capture has a total of %s frames at %s frames per second", self.total_frames, self.fps)
+
         if self.current_frame < self.total_frames:
             logging.info("Reading frame %s", self.current_frame)
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frame)
             ret, frame = self.cap.read()
             if ret:
                 self.current_frame += self.fps * seconds_to_skip
-                x1, y1 = min(self.roi_start[0], self.roi_end[0]), min(self.roi_start[1], self.roi_end[1])
-                x2, y2 = max(self.roi_start[0], self.roi_end[0]), max(self.roi_start[1], self.roi_end[1])
+                x1, y1 = min(self.roi_start[0], self.roi_end[0]), min(
+                    self.roi_start[1], self.roi_end[1])
+                x2, y2 = max(self.roi_start[0], self.roi_end[0]), max(
+                    self.roi_start[1], self.roi_end[1])
                 return frame[y1:y2, x1:x2]
             else:
                 logging.info("Unable to read frame")
@@ -142,7 +150,6 @@ class VideoCapture():
             self.cap.release()
             return None
 
-    
 
 if __name__ == "__main__":
     vc = VideoCapture(path='rec_2023-12-17_18-02.mp4')
